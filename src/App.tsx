57ae28dd3250f42b052d7b5cde05b191a6ea297e
yoppei0818 <yoppei0818@gmail.com>
2024-12-14 17:53:15 +0900

    開発環境の構築

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export const App = () => {
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+    </div>
+  );
+};
