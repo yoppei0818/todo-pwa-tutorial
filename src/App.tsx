2428c03b7c1abe75813ed7c239f2977b1637f558
yoppei0818 <yoppei0818@gmail.com>
2024-12-15 13:28:49 +0900

    12/15分の学習記録

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,105 @@
+import { useState } from "react";
+
+type Todo = {
+  value: string;
+  readonly id: number;
+  checked: boolean;
+  removed: boolean;
+};
+
 export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  //　追加前todoの文言変更
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  //　追加後todoの文言変更
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, value };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  };
+  //　todoの完了 or 未完了フラグ更新
+  const handleCheck = (id: number, checked: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, checked };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  };
+  //　todoの削除
+  const handleRemove = (id: number, removed: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, removed };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  };
+  //　todoの追加
+  const handleSubmit = () => {
+    if (!text) return;
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false,
+    };
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
-        />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={handleChange} />
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              disabled={todo.removed}
+              checked={todo.checked}
+              onChange={() => handleCheck(todo.id, !todo.checked)}
+            />
+            <input
+              type="text"
+              disabled={todo.checked || todo.removed}
+              value={todo.value}
+              onChange={(e) => {
+                handleEdit(todo.id, e.target.value);
+              }}
+            />
+            <button onClick={() => handleRemove(todo.id, !todo.removed)}>
+              {todo.removed ? "復元" : "削除"}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <p>{text}</p>
     </div>
   );
 };
